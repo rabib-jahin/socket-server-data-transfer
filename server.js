0ef8062426014ee93db5070b5abe312c9d87c711
rabib-jahin <rabibjahin2000@gmail.com>
2021-02-10 16:48:50 +0600

    sec

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const app=require('express')();
 const http=require('http').createServer(app);
-const io=require('socket.io')(http,{ wsEngine: 'ws' })
-const cors=require('cors');
+const io=require('socket.io')(http,{
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+})
+
 let room;
 const port=process.env.PORT||5000;
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
+
 
 io.on('connection',socket=>{
 
