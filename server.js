4a9674e07006a6ac2d983ff228180ffa8d61106f
rabib-jahin <rabibjahin2000@gmail.com>
2021-02-10 14:41:46 +0600

    sec

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app=require('express')();
 const http=require('http').createServer(app);
-const io=require('socket.io')(http)
+const io=require('socket.io')(http,{ wsEngine: 'ws' })
 const cors=require('cors');
+let room;
 const port=process.env.PORT||5000;
-
 
 
 io.on('connection',socket=>{
@@ -11,8 +11,22 @@ io.on('connection',socket=>{
 console.log("connected "+socket.id)
 socket.on("canvas-data",data=>{
 
-socket.broadcast.emit("canvas-data",data);
+socket.broadcast.emit("canvas-data",data.img);
+
+
 })
+socket.on('sendText',data=>{
+console.log("server sent" +data.data)
+    socket.to(data.room).broadcast.emit("sendText",data.data);
+})
+socket.on('create',data=>{
+    console.log(data+" joined")
+room=data;
+socket.join(data);
+
+
+})
+
 
 })
 http.listen(port,()=>{
