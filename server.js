999a15da031f50d607b5246d1ff981f14432f4ab
rabib-jahin <rabibjahin2000@gmail.com>
2021-02-10 16:36:02 +0600

    sec

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const io=require('socket.io')(http,{ wsEngine: 'ws' })
 const cors=require('cors');
 let room;
 const port=process.env.PORT||5000;
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 io.on('connection',socket=>{
 
