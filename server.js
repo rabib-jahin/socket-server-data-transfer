d0e283c0855f0202c361d276839a66bf844fdf0c
rabib-jahin <rabibjahin2000@gmail.com>
2021-02-10 17:11:11 +0600

    sec

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const io=require('socket.io')(http,{
     handlePreflightRequest: (req, res) => {
         const headers = {
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Origin": "https://elastic-hoover-cd7c79.netlify.app", //or the specific origin you want to give access to,
             "Access-Control-Allow-Credentials": true
         };
         res.writeHead(200, headers);
